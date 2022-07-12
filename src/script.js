950394e081e27a99e4f4d6131ec0b40a17112279
30isnottoolate <akos.varga.92@gmail.com>
2022-07-12 18:32:59 +0200

    Add basic app structure
        Added required elements to app, made it stateful. Implemented blank
        handleClick, connected it to buttons.

--- src/script.js
+++ src/script.js
@@ -3,10 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './app_styles.css';
 
 class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    console.log("click");
+  }
+
   render() {
     return (
-      <div id="app">
-        <p>Hit it!</p>
+      <div id="drum-machine">
+        <div id="display"><p id="displayText"></p></div> 
+        <div id="buttons">
+          <button id="kick" className="drum-pad" onClick={this.handleClick}>Q
+          </button>
+          <button id="snare" className="drum-pad" onClick={this.handleClick}>W
+          </button>
+          <button id="rimshot" className="drum-pad" onClick={this.handleClick}>E
+          </button>
+          <button id="hhclosed" className="drum-pad" onClick={this.handleClick}>A
+          </button>
+          <button id="hhopen" className="drum-pad" onClick={this.handleClick}>S
+          </button>
+          <button id="tom" className="drum-pad" onClick={this.handleClick}>D
+          </button>
+          <button id="crash" className="drum-pad" onClick={this.handleClick}>Z
+          </button>
+          <button id="china" className="drum-pad" onClick={this.handleClick}>X
+          </button>
+          <button id="cowbell" className="drum-pad" onClick={this.handleClick}>C
+          </button>
+        </div>
       </div>
     );
   }
