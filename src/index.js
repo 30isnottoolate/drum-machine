3abc68e33e249d57befc959e29817fa26c114711
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-24 11:47:03 +0200

    Render DrumMachine from index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './script.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DrumMachine from './DrumMachine';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <DrumMachine />
+  </React.StrictMode>
+);
